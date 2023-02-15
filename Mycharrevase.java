e94508a4056ef5e1efcc11f068ccf70be709d322
Jayanthi <120365823+j51277@users.noreply.github.com>
2023-02-15 11:38:34 +0530

    Add files via upload

---
+++ Mycharrevase.java
@@ -0,0 +1,13 @@
+public class Mycharrevase
+{
+public static void main(String args[])
+{
+char a[]={'j','a','v','a'};
+String x="";
+for(int i=a.length-1;i>=0;i--)
+{
+x=x+a[i];
+}
+System.out.println(x);
+}
+}
